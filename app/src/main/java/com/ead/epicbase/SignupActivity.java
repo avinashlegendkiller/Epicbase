0b4ceb1073bec8df7c403f908553ca4bd267994d
Avinash Gardas <Avinash Gardas>
2017-02-09 09:44:34 +0530

    first commit

---
+++ app/src/main/java/com/ead/epicbase/SignupActivity.java
@@ -0,0 +1,99 @@
+package com.ead.epicbase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private Button btnSignIn, btnSignUp, btnResetPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+    String email, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        //Get Firebase auth instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btnSignIn = (Button) findViewById(R.id.sign_in_button);
+        btnSignUp = (Button) findViewById(R.id.sign_up_button);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
+
+        //signUp
+
+    }
+
+    public void resetPassword(View view) {
+        startActivity(new Intent(SignupActivity.this, ResetPasswordActivity.class));
+    }
+
+    public void signIn(View view) {
+        finish();
+    }
+
+    public void signUp(View view) {
+        email = inputEmail.getText().toString().trim();
+        password = inputPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)) {
+            Snackbar.make(view, "Enter email address!", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show();
+        }
+
+        if(TextUtils.isEmpty(password)) {
+            Snackbar.make(view, "Enter email address!", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show();
+        }
+
+        if(password.length() < 6) {
+            Snackbar.make(view, "Password too short, enter minimum 6 characters!", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_signup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
